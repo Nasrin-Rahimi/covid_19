df0c5777e5e26bba766adfe51f2aceb35b71b971
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-19 11:28:40 +0000

    edit cli

--- lib/covid_19/cli.rb
+++ lib/covid_19/cli.rb
@@ -10,15 +10,33 @@ class Covid19::CLI
   
   def greeting
     puts "Welcome to Covid19 app"
+    puts "Please enter a numbre"
   end
 
   def list_countries
   end
   
   def menu
+    input = nil
+    
+    while input != "exit" do
+      
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "you entered 1"
+      when "2"
+        puts "you entered 2"
+      else 
+        puts "Not sure what you want, type list or exit"
+      end
+      
+    end
+    
   end
 
   def goodbye
+    puts "bybye"
   end
     
 end
